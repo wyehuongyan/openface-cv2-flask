fd44025153fd1a6008a28476d4c9f1b122b69f59
Yan Wye Huong <wyehuong88@gmail.com>
2016-08-22 12:20:56 +0800

    added gunicorn run command.

--- flask/run.py
+++ flask/run.py
@@ -5,3 +5,5 @@ if __name__ == '__main__': # code is being loaded directly
 	app.debug = False
 	app.threaded = True
 	app.run(host='0.0.0.0')
+
+# $ gunicorn --chdir ./flask run:app --timeout 31104000 -k gevent -w 3 -b 0.0.0.0:5000   
